4ecbbe005dcdebb195f1e9bc1d4b6c3e15ff6714
Frank Beckmann <fbeckman@t-online.de>
2023-05-04 16:13:19 +0200

    Credentials skeleton
    Fill with your own credentials

---
+++ ESPNowLoRaGateway/include/Credentials.h
@@ -0,0 +1,14 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// WiFi parameters
+const char* ssid = "YOUR SSID";
+const char* password = "YOUR WIFI PASSWORD";
+
+// Telegram bot credentials
+#define botToken "YOUR TELEGRAM BOT TOKEN"
+#define userID "YOUR TELEGRAM USERID"
+
+#define INFLUXDB_HOST "YOUR INFLUXDB HOST IP ADDRESS"
+
+#endif
