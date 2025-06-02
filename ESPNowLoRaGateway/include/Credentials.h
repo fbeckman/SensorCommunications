98137c5873f48fb05eebadfd77531b5d21de04de
Frank Beckmann <fbeckman@t-online.de>
2025-06-02 18:11:12 +0200

    Update Credentials.h

--- ESPNowLoRaGateway/include/Credentials.h
+++ ESPNowLoRaGateway/include/Credentials.h
@@ -8,10 +8,6 @@ const char* password1 = "YOUR WIFI PASSWORD1";
 const char* ssid2 = "YOUR SSID2";
 const char* password1 = "YOUR WIFI PASSWORD2";
 
-// Telegram bot credentials
-#define botToken "YOUR TELEGRAM BOT TOKEN"
-#define userID "YOUR TELEGRAM USERID"
-
 #define INFLUXDB_HOST "YOUR INFLUXDB HOST IP ADDRESS"
 
 #endif
